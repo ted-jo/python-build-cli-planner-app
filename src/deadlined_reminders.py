e86ce235dbaf02e78755575a3b61ccd7263153f5
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-11 22:12:57 -0500

    Fixed iter typo

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -33,10 +33,11 @@ class DateReminder(DeadlinedReminder):
     def __init__(self, text, date):
         self.text = text
         self.date = parse(date, dayfirst=True)
+
+    def __iter__(self):
+        return iter([self.text, self.date.isoformat()])   
         
     
     def is_due(self):
         return self.date <= datetime.now()
 
-    def __iter_(self):
-        return iter([self.text, self.date.isoformat()])
